38d68af44fdc5aed70d200296641242ea82276e5
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-15 19:49:39 +0800

    Initial Commit

---
+++ chat/chatApp.swift
@@ -0,0 +1,17 @@
+//
+//  chatApp.swift
+//  chat
+//
+//  Created by Yuki Honda on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct chatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
