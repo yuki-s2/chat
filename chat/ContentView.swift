4a7162c6d0469da0f6e545f5f9da631ab7f7acdf
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-16 14:24:48 +0800

    チャット画面作成

--- chat/ContentView.swift
+++ chat/ContentView.swift
@@ -9,13 +9,121 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ScrollView {
+            VStack {
+                HStack(alignment: .top) {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Spacer()
+                }
+                HStack (alignment: .top) {
+                    Spacer()
+                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                }
+                HStack(alignment: .top) {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Spacer()
+                }
+                HStack (alignment: .top) {
+                    Spacer()
+                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                }
+                HStack(alignment: .top) {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Spacer()
+                }
+                HStack (alignment: .top) {
+                    Spacer()
+                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                }
+                HStack(alignment: .top) {
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Spacer()
+                }
+                HStack (alignment: .top) {
+                    Spacer()
+                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                        .foregroundColor(.white)
+                        .padding()
+                        .frame(width: 240)
+                        .background (
+                            Rectangle()
+                                .cornerRadius(10)
+                        )
+                    Circle()
+                        .frame(width: 40, height: 40)
+                    
+                }
+            }
+
         }
         .padding()
+
     }
 }
 
