bf163da75f5e2ad2f6f9517fdd503f8ca9221a98
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-16 15:01:57 +0800

    TextField作成

--- chat/ContentView.swift
+++ chat/ContentView.swift
@@ -8,122 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var isFocused: Bool
+    @State private var name = ""
+    
     var body: some View {
-        ScrollView {
-            VStack {
-                HStack(alignment: .top) {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Spacer()
+        VStack {
+            ScrollView {
+                VStack {
+                    HStack(alignment: .top) {
+                        Circle()
+                            .frame(width: 40, height: 40)
+                        Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 240)
+                            .background (
+                                Rectangle()
+                                    .cornerRadius(10)
+                            )
+                        Spacer()
+                    }
+                    HStack (alignment: .top) {
+                        Spacer()
+                        Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 240)
+                            .background (
+                                Rectangle()
+                                    .cornerRadius(10)
+                            )
+                        Circle()
+                            .frame(width: 40, height: 40)
+                        
+                    }
+                    HStack(alignment: .top) {
+                        Circle()
+                            .frame(width: 40, height: 40)
+                        Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 240)
+                            .background (
+                                Rectangle()
+                                    .cornerRadius(10)
+                            )
+                        Spacer()
+                    }
+                    HStack (alignment: .top) {
+                        Spacer()
+                        Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
+                            .foregroundColor(.white)
+                            .padding()
+                            .frame(width: 240)
+                            .background (
+                                Rectangle()
+                                    .cornerRadius(10)
+                            )
+                        Circle()
+                            .frame(width: 40, height: 40)
+                        
+                    }
                 }
-                HStack (alignment: .top) {
-                    Spacer()
-                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    
-                }
-                HStack(alignment: .top) {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Spacer()
-                }
-                HStack (alignment: .top) {
-                    Spacer()
-                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    
-                }
-                HStack(alignment: .top) {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Spacer()
-                }
-                HStack (alignment: .top) {
-                    Spacer()
-                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    
-                }
-                HStack(alignment: .top) {
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Spacer()
-                }
-                HStack (alignment: .top) {
-                    Spacer()
-                    Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                        .foregroundColor(.white)
-                        .padding()
-                        .frame(width: 240)
-                        .background (
-                            Rectangle()
-                                .cornerRadius(10)
-                        )
-                    Circle()
-                        .frame(width: 40, height: 40)
-                    
-                }
+                .padding()
             }
+            
+            HStack {
+                TextField("あなたの名前", text: $name)
+                    .padding()
+                    .focused($isFocused)
+                Button {
+                    print("\(name)を送信しました")
+                    isFocused = false
+                } label: {
+                    Image(systemName: "arrow.up.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundColor(Color.blue)
+                }
 
+            }
+            .padding(.horizontal)
         }
-        .padding()
-
     }
 }
 
