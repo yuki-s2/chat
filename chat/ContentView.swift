93b0c51ad62d09c598a54463add27a28c08ae165
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-17 08:27:18 +0800

    チャット機能追加

--- chat/ContentView.swift
+++ chat/ContentView.swift
@@ -7,88 +7,105 @@
 
 import SwiftUI
 
+struct Background<Content: View>: View {
+    private var content: Content
+    
+    init(@ViewBuilder content: @escaping () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        Color.white
+            .overlay(content)
+    }
+}
+
 struct ContentView: View {
     @FocusState private var isFocused: Bool
     @State private var name = ""
+    @State private var messages: [Message] = [.init(id: .init(), text: "こんにちは！何か喋って", isMyMessage: false)]
+    
+    struct Message: Identifiable {
+        let id: UUID
+        let text: String
+        let isMyMessage: Bool
+    }
     
     var body: some View {
-        VStack {
-            ScrollView {
-                VStack {
-                    HStack(alignment: .top) {
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(width: 240)
-                            .background (
-                                Rectangle()
-                                    .cornerRadius(10)
-                            )
-                        Spacer()
+        Background {
+            VStack {
+                ScrollView {
+                    VStack {
+                        ForEach(messages) { message in
+                            if message.isMyMessage {
+                                HStack (alignment: .top) {
+                                    Spacer()
+                                    Text(message.text)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .frame(width: 240)
+                                        .background (
+                                            Rectangle()
+                                                .cornerRadius(10)
+                                        )
+                                    Circle()
+                                        .frame(width: 40, height: 40)
+                                    
+                                }
+                            } else {
+                                HStack(alignment: .top) {
+                                    Circle()
+                                        .frame(width: 40, height: 40)
+                                    Text(message.text)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .frame(width: 240)
+                                        .background (
+                                            Rectangle()
+                                                .cornerRadius(10)
+                                        )
+                                    Spacer()
+                                }
+                            }
+                        }
                     }
-                    HStack (alignment: .top) {
-                        Spacer()
-                        Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(width: 240)
-                            .background (
-                                Rectangle()
-                                    .cornerRadius(10)
-                            )
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        
-                    }
-                    HStack(alignment: .top) {
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        Text("Hello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, SwiftHello, Swiftfffffffffffffffffffffffffffffffff")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(width: 240)
-                            .background (
-                                Rectangle()
-                                    .cornerRadius(10)
-                            )
-                        Spacer()
-                    }
-                    HStack (alignment: .top) {
-                        Spacer()
-                        Text("Hello, Swift.cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10).cornerRadius(10)(alignment: .top)(alignment: .top)")
-                            .foregroundColor(.white)
-                            .padding()
-                            .frame(width: 240)
-                            .background (
-                                Rectangle()
-                                    .cornerRadius(10)
-                            )
-                        Circle()
-                            .frame(width: 40, height: 40)
-                        
-                    }
-                }
-                .padding()
-            }
-            
-            HStack {
-                TextField("あなたの名前", text: $name)
                     .padding()
-                    .focused($isFocused)
-                Button {
-                    print("\(name)を送信しました")
-                    isFocused = false
-                } label: {
-                    Image(systemName: "arrow.up.circle.fill")
-                        .resizable()
-                        .frame(width: 30, height: 30)
-                        .foregroundColor(Color.blue)
                 }
+                
+                HStack {
+                    TextField("あなたの名前", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                        .padding()
+                        .onSubmit {   // ⬅︎  クロージャ内の処理を実行
 
+                            print("入力された値: \($name)")
+                            
+                        } // onSubmit
+                        .focused($isFocused)
+                    Button {
+                        print("\(name)を送信しました")
+                        let newMessage = Message(id: .init(), text: name, isMyMessage: true)
+                        messages.append(newMessage)
+                        isFocused = false
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            let responseMessage = Message(id: .init(), text: "受け取りました！", isMyMessage: false)
+                            messages.append(responseMessage)
+                        }
+                        
+                    } label: {
+                        Image(systemName: "arrow.up.circle.fill")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .foregroundColor(Color.blue)
+                    }
+                    
+                }
+                .padding(.horizontal)
             }
-            .padding(.horizontal)
+        }
+        .onTapGesture {
+            isFocused = false
         }
     }
 }
