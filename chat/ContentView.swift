acd56e70f213c08db0688e3db477f3d998a89d13
Yuki Honda <dahlia.15s2@gmail.com>
2023-09-18 09:59:35 +0800

    文章読み上げ機能追加中

--- chat/ContentView.swift
+++ chat/ContentView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AVFoundation
+
 
 struct Background<Content: View>: View {
     private var content: Content
@@ -22,8 +24,11 @@ struct Background<Content: View>: View {
 
 struct ContentView: View {
     @FocusState private var isFocused: Bool
-    @State private var name = ""
-    @State private var messages: [Message] = [.init(id: .init(), text: "こんにちは！何か喋って", isMyMessage: false)]
+    @State private var textFieldValue = ""
+    @State private var messages: [Message] = [
+        .init(id: .init(), text: "こんにちは！何か喋って", isMyMessage: false)
+    ]
+    
     
     struct Message: Identifiable {
         let id: UUID
@@ -32,80 +37,102 @@ struct ContentView: View {
     }
     
     var body: some View {
-        Background {
-            VStack {
-                ScrollView {
-                    VStack {
-                        ForEach(messages) { message in
-                            if message.isMyMessage {
-                                HStack (alignment: .top) {
-                                    Spacer()
-                                    Text(message.text)
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .frame(width: 240)
-                                        .background (
-                                            Rectangle()
-                                                .cornerRadius(10)
-                                        )
-                                    Circle()
-                                        .frame(width: 40, height: 40)
-                                    
-                                }
-                            } else {
-                                HStack(alignment: .top) {
-                                    Circle()
-                                        .frame(width: 40, height: 40)
-                                    Text(message.text)
-                                        .foregroundColor(.white)
-                                        .padding()
-                                        .frame(width: 240)
-                                        .background (
-                                            Rectangle()
-                                                .cornerRadius(10)
-                                        )
-                                    Spacer()
-                                }
-                            }
-                        }
-                    }
-                    .padding()
-                }
-                
-                HStack {
-                    TextField("あなたの名前", text: $name)
-                        .textFieldStyle(.roundedBorder)
-                        .padding()
-                        .onSubmit {   // ⬅︎  クロージャ内の処理を実行
+        Button {
+            speak(text: "Hello")
+        } label: {
+            Text("Speak")
+        }
 
-                            print("入力された値: \($name)")
-                            
-                        } // onSubmit
-                        .focused($isFocused)
-                    Button {
-                        print("\(name)を送信しました")
-                        let newMessage = Message(id: .init(), text: name, isMyMessage: true)
-                        messages.append(newMessage)
-                        isFocused = false
-                        
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            let responseMessage = Message(id: .init(), text: "受け取りました！", isMyMessage: false)
-                            messages.append(responseMessage)
-                        }
-                        
-                    } label: {
-                        Image(systemName: "arrow.up.circle.fill")
-                            .resizable()
-                            .frame(width: 30, height: 30)
-                            .foregroundColor(Color.blue)
-                    }
-                    
-                }
-                .padding(.horizontal)
+//        Background {
+//            VStack {
+//                ScrollView {
+//                    VStack {
+//                        ForEach(messages) { message in
+//                            messageRow(message)
+//                        }
+//                    }
+//                    .padding()
+//                }
+//
+//                HStack {
+//                    TextField("", text: $textFieldValue)
+//                        .textFieldStyle(.roundedBorder)
+//                        .padding()
+//                        .focused($isFocused)
+//                    Button {
+//                        let newMessage = Message(id: .init(), text: textFieldValue, isMyMessage: true)
+//                        messages.append(newMessage)
+//                        isFocused = false
+//                        speak(text: "Hello, Yuki!")
+//
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+//                            let responseMessage = Message(id: .init(), text: "受け取りました！", isMyMessage: false)
+//                            messages.append(responseMessage)
+//                        }
+//
+//                    } label: {
+//                        Image(systemName: "arrow.up.circle.fill")
+//                            .resizable()
+//                            .frame(width: 30, height: 30)
+//                            .foregroundColor(Color.blue)
+//                    }
+//
+//                }
+//                .padding(.horizontal)
+//            }
+//        }
+//        .onTapGesture {
+//            isFocused = false
+//        }
+    }
+    
+    private func messageRow(_ message: Message) -> some View {
+        HStack (alignment: .top) {
+            if message.isMyMessage {
+                Spacer()
+                Text(message.text)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 240)
+                    .background (
+                        Rectangle()
+                            .cornerRadius(10)
+                    )
+                Circle()
+                    .frame(width: 40, height: 40)
+            } else {
+                Circle()
+                    .frame(width: 40, height: 40)
+                Text(message.text)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(width: 240)
+                    .background (
+                        Rectangle()
+                            .cornerRadius(10)
+                    )
+                Spacer()
             }
         }
-        .onTapGesture {
-            isFocused = false
+    }
+    
+    func speak(text: String) {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("speak failed: \(error)")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+            let synthesizer = AVSpeechSynthesizer()
+            let utterance = AVSpeechUtterance(string: "text")
+            utterance.voice = .init(language: "en-US") // 言語を設定する
+            utterance.volume = 0.5
+            utterance.rate = 0.5
+            utterance.pitchMultiplier = 1.2
+            synthesizer.speak(utterance)
+            print("speak text")
         }
     }
 }
